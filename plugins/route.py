016ce5dc2250096b75b0d34757569aeedecfd807
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-01-02 13:38:35 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Join Telegram
+    return web.json_response("Join Telegram @MX_Networks")
