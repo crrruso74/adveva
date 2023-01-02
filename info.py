5ee69ac06d53694a7d7bd5fcebbf6b340089e025
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-01-02 14:58:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pushpa:pushpa@cluster0.tosijto.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://userdb:userdb@cluster0.szkfqrs.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "files")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files_cloud')
 
