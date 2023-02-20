de25c6cc0ae3d7f87a81ded6e800e6efd0b276b0
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-02-20 16:07:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://net:net@cluster0.8ztiiu0.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Data")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
